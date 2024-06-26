04c805fe757f2919a944b29dc383378ec95e21d8
m <127431263+monserratdb@users.noreply.github.com>
2024-06-26 16:49:45 -0400

    Update main1.py

--- main1.py
+++ main1.py
@@ -2,28 +2,29 @@ import gurobipy as gp
 from gurobipy import GRB
 import pandas as pd
 
+# CORRER EL CODIGO EN LA MISMA CARPETA EN QUE SE TIENE muestras.csv
 # Cargar datos
 data = pd.read_csv('muestras.csv')
-X = data.iloc[:, :-1].values  # Características
-Y = data.iloc[:, -1].values  # Variable objetivo
+X = data.iloc[:, :-1].values  # características
+Y = data.iloc[:, -1].values  # variable obj
 
-# Crear el modelo
+# crear el modelo
 model = gp.Model()
 
-# Crear variables para los pesos y la constante, con restricciones no negativas si es necesario
+# crear variables para los pesos y la constante, se pone -GRB.INFINITY porque pueden ser negativos los pesos
 w = model.addVars(len(X[0]), lb=-GRB.INFINITY, name="w")
 b = model.addVar(lb=-GRB.INFINITY, name="b")
 
-# Crear la función objetivo
+# crear la f.o
 obj = sum((b + gp.quicksum(w[i] * X[r][i] for i in range(len(X[0]))) - Y[r]) ** 2 for r in range(len(Y)))
 model.setObjective(obj, GRB.MINIMIZE)
 
-# Optimizar el modelo
+# optimizar
 model.optimize()
 
-# Verificar si la solución es óptima
+# verificar si la solución es óptima
 if model.status == GRB.OPTIMAL:
-    # Imprimir los resultados
+    # print resultados
     for i in range(len(X[0])):
         print(f"w_{i+1} (característica {data.columns[i]}): {w[i].X}")
     print(f"b (constante): {b.X} gramos")
